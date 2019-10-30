5b292f54df1d92f2e67a3aacd9afe226596826f7
alexawl <alexawl@bellsouth.net>
2019-10-30 21:47:47 +0000

    first commit

--- public/bundle.js
+++ public/bundle.js
@@ -701,30 +701,54 @@ __webpack_require__.r(__webpack_exports__);
  *  This function is called every time the user changes types or changes any input
  */
 function render(variables = {}) {
-  // here we ask the logical questions to make decitions on how to build the heml
+  // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let name = "Lucy";
+  if (variables.name !== null) name = variables.name;
+  let lastName = "Boilett";
+  if (variables.lastname !== null) lastName = variables.lastname;
+
+  let twitter = "";
+  if (variables.twitter !== null) twitter = variables.twitter;
+  let instagram = "";
+  if (variables.instagram !== null) instagram = variables.instagram;
+  let linkedin = "";
+  if (variables.linkedin !== null) linkedin = variables.linkedin;
+  let github = "";
+  if (variables.github !== null) github = variables.github;
+
+  let Position = "position-left";
+  if (variables.socialMediaPosition !== "left") Position = variables.socialMediaPosition;
+
+  let country = "USA";
+  if (variables.country !== null) country = variables.country;
+  let city = "Miami";
+  if (variables.city !== null) city = variables.city;
+
+  let role = "Web Developer";
+  if (variables.role !== null) role = variables.role;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/ln/${linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
 /**
- * Ignore this lines below, here is where we do the logic for the dropdowns
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
   window.variables = {
--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,44 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let name = "Lucy";
+  if (variables.name !== null) name = variables.name;
+  let lastName = "Boilett";
+  if (variables.lastname !== null) lastName = variables.lastname;
+
+  let twitter = "";
+  if (variables.twitter !== null) twitter = variables.twitter;
+  let instagram = "";
+  if (variables.instagram !== null) instagram = variables.instagram;
+  let linkedin = "";
+  if (variables.linkedin !== null) linkedin = variables.linkedin;
+  let github = "";
+  if (variables.github !== null) github = variables.github;
+
+  let Position = "position-left";
+  if (variables.socialMediaPosition !== "left")
+    Position = variables.socialMediaPosition;
+
+  let country = "USA";
+  if (variables.country !== null) country = variables.country;
+  let city = "Miami";
+  if (variables.city !== null) city = variables.city;
+
+  let role = "Web Developer";
+  if (variables.role !== null) role = variables.role;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/ln/${linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
